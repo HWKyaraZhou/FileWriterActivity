8fac9ebe18fdb1e88826066f02d82325f568bfc8
Kyara Zhou <kzhou1@hwemail.com>
2024-09-08 23:47:21 -0700

    Add hidden and non-hidden files using chosen writing method; create .gitignore file

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -5,43 +5,28 @@ import java.nio.charset.StandardCharsets;
 public class MyFileWriter {
     public static void main(String[] args) {
         String data = "Hello, World!";
-        String fileName1 = "example.txt";
+        String fileName1;
         String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
-
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         // 2. Using BufferedWriter
+
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
             bufferedWriter.write(data);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
+        
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(".notAPassword.txt"))) {
+            bufferedWriter.write("password");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 5. Using Files (java.nio.file)
-        try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+        File file = new File(".topSecret");
+        file.mkdirs();
+        Path filePath = Paths.get(".topSecret", "classified");
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath.toFile()))) {
+            bufferedWriter.write("too bad not telling you any secrets");
         } catch (IOException e) {
             e.printStackTrace();
         }
